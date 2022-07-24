82584c2b1b7d385879ed1a37d467024102afeda1
Kevin <kevin@2046inc.xyz>
2022-07-23 22:22:48 -0400

    Update windowsterminal.py

--- pokemonterminal/terminal/adapters/windowsterminal.py
+++ pokemonterminal/terminal/adapters/windowsterminal.py
@@ -7,7 +7,7 @@ from . import TerminalProvider as _TProv
 class WindowsTerminalProvider(_TProv):
 
     def set_background_image(path: str):
-        profiles_path = os.environ['LOCALAPPDATA'] + '\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState\\settings.json'
+        profiles_path = os.environ['LOCALAPPDATA'] + '/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json'
         with open(profiles_path, 'r+', encoding='utf8') as json_file:
             # read profiles.json
             # remove comments from json to load
@@ -27,6 +27,8 @@ class WindowsTerminalProvider(_TProv):
                 del profile['backgroundImage']
             else:
                 profile['backgroundImage'] = path
+                wsl_path = os.environ['WSLPATH']
+                profile['backgroundImage'] = wsl_path + path
 
             # write to file
             # it lost orignal indent, comment, ...
